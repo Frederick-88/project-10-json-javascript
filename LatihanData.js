6b20e92ebe04ec2299bae75e58fd59038578b9c1
Frederick-88 <frederickfd88@gmail.com>
2020-04-01 16:41:09 +0700

    progress hari ini

---
+++ LatihanData.js
@@ -0,0 +1,164 @@
+const DATA = {
+    "employees":[
+        {"firstName":"John","lastName":"Doe"},
+        {"firstName":"Anna","lastName":"Smith"},
+        {"firstName":"Peter","lastName":"Jones"},
+        {"firstName":"Doni","lastName":"Wong"},
+        {"firstName":"Slamet","lastName":"Jordan"},
+        {"firstName":"Carlo","lastName":"Wee"},
+        {"firstName":"Philips","lastName":"Morris"},
+        {"firstName":"Angga","lastName":"Dwi"},
+        {"firstName":"Vicky","lastName":"Sasongko"},
+        {"firstName":"Nadya","lastName":"Lembayung"},
+    ],
+    "jobs":[
+        {"department": "QC", "title": "QA"},
+        {"department": "IT", "title": ['IT Support', 'Software engineer', 'UI/UX', 'DevOps', 'Tester']},
+        {"department": "Human Resources Development", "title": "SPV"},
+        {"department": "Finance and Accounting", "title": "Finance"},
+        {"department": "Marketing dan Customer Service", "title": "marketing"},
+        {"department": "Maintenance and Facility", "title": "maintenance"},
+    ],
+    "details": [
+        {"age": [22, 23, 24, ,29, 30, 32 ]},
+        {"experience": ['2 Tahun', '3 Tahun', '4 Tahun',]}
+    ],
+    // PRINT DATA
+        printList1: () => {
+            let employeeName = DATA.employees[0] .firstName
+            let employeeName2 = DATA.employees[0] .lastName
+            let employeeJob = DATA.jobs[1].department
+            let employeeTitle = DATA.jobs[1].title[2]
+            let employeeAge = DATA.details[0].age[1]
+            let employeeExperience = DATA.details[1].experience[0]
+
+            console.log(`name: ${employeeName} ${employeeName2}, jobs: department: ${employeeJob}, title: ${employeeTitle}, age: ${employeeAge}, Experience: ${employeeExperience}`);
+            
+        },
+
+        printList2: () => {
+            let employeeName = DATA.employees[1] .firstName
+            let employeeName2 = DATA.employees[1] .lastName
+            let employeeJob = DATA.jobs[2].department
+            let employeeTitle = DATA.jobs[2].title
+            let employeeAge = DATA.details[0].age[3]
+            let employeeExperience = DATA.details[1].experience[2]
+
+            console.log(`name: ${employeeName} ${employeeName2}, jobs: department: ${employeeJob}, title: ${employeeTitle}, age: ${employeeAge}, Experience: ${employeeExperience}`);
+            
+        },
+        printList3: () => {
+            let employeeName = DATA.employees[2] .firstName
+            let employeeName2 = DATA.employees[2] .lastName
+            let employeeJob = DATA.jobs[1].department
+            let employeeTitle = DATA.jobs[1].title[3]
+            let employeeAge = DATA.details[0].age[2]
+            let employeeExperience = DATA.details[1].experience[1]
+
+            console.log(`name: ${employeeName} ${employeeName2}, jobs: department: ${employeeJob}, title: ${employeeTitle}, age: ${employeeAge}, Experience: ${employeeExperience}`);
+            
+        },
+        printList4: () => {
+            let employeeName = DATA.employees[3] .firstName
+            let employeeName2 = DATA.employees[3] .lastName
+            let employeeJob = DATA.jobs[1].department
+            let employeeTitle = DATA.jobs[1].title[4]
+            let employeeAge = DATA.details[0].age[0]
+            let employeeExperience = DATA.details[1].experience[0]
+
+            console.log(`name: ${employeeName} ${employeeName2}, jobs: department: ${employeeJob}, title: ${employeeTitle}, age: ${employeeAge}, Experience: ${employeeExperience}`);
+            
+        },
+        printList5: () => {
+            let employeeName = DATA.employees[4] .firstName
+            let employeeName2 = DATA.employees[4] .lastName
+            let employeeJob = DATA.jobs[1].department
+            let employeeTitle = DATA.jobs[1].title[4]
+            let employeeAge = DATA.details[0].age[0]
+            let employeeExperience = DATA.details[1].experience[0]
+
+            console.log(`name: ${employeeName} ${employeeName2}, jobs: department: ${employeeJob}, title: ${employeeTitle}, age: ${employeeAge}, Experience: ${employeeExperience}`);
+            
+        },
+        printList6: () => {
+            let employeeName = DATA.employees[5] .firstName
+            let employeeName2 = DATA.employees[5] .lastName
+            let employeeJob = DATA.jobs[3].department
+            let employeeTitle = DATA.jobs[1].title[4]
+            let employeeAge = DATA.details[0].age[0]
+            let employeeExperience = DATA.details[1].experience[0]
+
+            console.log(`name: ${employeeName} ${employeeName2}, jobs: department: ${employeeJob}, title: ${employeeTitle}, age: ${employeeAge}, Experience: ${employeeExperience}`);
+            
+        },
+        printList7: () => {
+            let employeeName = DATA.employees[6] .firstName
+            let employeeName2 = DATA.employees[6] .lastName
+            let employeeJob = DATA.jobs[5].department
+            let employeeTitle = DATA.jobs[1].title[4]
+            let employeeAge = DATA.details[0].age[0]
+            let employeeExperience = DATA.details[1].experience[0]
+
+            console.log(`name: ${employeeName} ${employeeName2}, jobs: department: ${employeeJob}, title: ${employeeTitle}, age: ${employeeAge}, Experience: ${employeeExperience}`);
+            
+        },
+        printList8: () => {
+            let employeeName = DATA.employees[7] .firstName
+            let employeeName2 = DATA.employees[7] .lastName
+            let employeeJob = DATA.jobs[1].department
+            let employeeTitle = DATA.jobs[1].title[4]
+            let employeeAge = DATA.details[0].age[0]
+            let employeeExperience = DATA.details[1].experience[0]
+
+            console.log(`name: ${employeeName} ${employeeName2}, jobs: department: ${employeeJob}, title: ${employeeTitle}, age: ${employeeAge}, Experience: ${employeeExperience}`);
+            
+        },
+        printList9: () => {
+            let employeeName = DATA.employees[8] .firstName
+            let employeeName2 = DATA.employees[8] .lastName
+            let employeeJob = DATA.jobs[0].department
+            let employeeTitle = DATA.jobs[1].title[4]
+            let employeeAge = DATA.details[0].age[0]
+            let employeeExperience = DATA.details[1].experience[0]
+
+            console.log(`name: ${employeeName} ${employeeName2}, jobs: department: ${employeeJob}, title: ${employeeTitle}, age: ${employeeAge}, Experience: ${employeeExperience}`);
+            
+        },
+        printList10: () => {
+            let employeeName = DATA.employees[9] .firstName
+            let employeeName2 = DATA.employees[9] .lastName
+            let employeeJob = DATA.jobs[1].department
+            let employeeTitle = DATA.jobs[1].title[4]
+            let employeeAge = DATA.details[0].age[0]
+            let employeeExperience = DATA.details[1].experience[0]
+
+            console.log(`name: ${employeeName} ${employeeName2}, jobs: department: ${employeeJob}, title: ${employeeTitle}, age: ${employeeAge}, Experience: ${employeeExperience}`);
+            
+        },
+
+        printAll: () => {
+            DATA.printList1()
+            DATA.printList2()
+            DATA.printList3()
+            DATA.printList4()
+            DATA.printList5()
+            DATA.printList6()
+            DATA.printList7()
+            DATA.printList8()
+            DATA.printList9()
+            DATA.printList10()
+        }
+        
+    }
+    // console.log(DATA.jobs[0].department);
+    // console.log(DATA.jobs[1].title[2]);
+
+
+    // let building = {
+    //     name: 'batam pos',
+    //     floor: 21,
+    //     printNameAndFloor: function () {
+    //         const name =this.name;
+    //         const floor = this.floor;
+    //         console.log(`${name} building have ${floor} floors.`);
+    //     }
--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-var info = "internet positif";
-console.log('berisi informasi developer');
-console.log("berisi informasi kpda dev ${info}");
-console.log(`brisi informasi ${info}`);
+// var info = "internet positif";
+// console.log('berisi informasi developer');
+// console.log("berisi informasi kpda dev ${info}");
+// console.log(`brisi informasi ${info}`);
 
-// window.alert("NOTE: SELAMAT ANDA MEMENANGKAN UNDIAN!")
+// // window.alert("NOTE: SELAMAT ANDA MEMENANGKAN UNDIAN!")
 
-// memberikan input nama yang bisa diinput penguna
-const name = window.prompt("sebutkan nama anda.", "andi");
-console.log(`nama saya adalah ${name}`);
-// memberikan input nama yang bisa diinput penguna
+// // memberikan input nama yang bisa diinput penguna
+// const name = window.prompt("sebutkan nama anda.", "andi");
+// console.log(`nama saya adalah ${name}`);
+// // memberikan input nama yang bisa diinput penguna
 
-// KONFIRMASI
-const confirmNama = window.confirm(`apa bner namamu ${name} ?`)
-if (confirmNama) {
-    console.log('silahkan masuk')
-} else {
-    console.log('ulangi lagi.')
-}
+// // KONFIRMASI
+// const confirmNama = window.confirm(`apa bner namamu ${name} ?`)
+// if (confirmNama) {
+//     console.log('silahkan masuk')
+// } else {
+//     console.log('ulangi lagi.')
+// }
 // KONFIRMASI
 
 // let multiple string penggunaan backtick (`)
@@ -42,44 +42,53 @@ if (confirmNama) {
 
 // PEMANGGILAN ARRAY
 let human = {
-    name: 'bean',
-    height: 200,
-    weight: 50,
-    live: ['batam', 'singapore', {
-        position: 'director',
-        location: 'batam center',
-    }],
-    pet: {
-        name: 'beanyjr',
-        height: 20,
-        weight: 20,
-        live: ['batam', 'singapore'],
-    },
-    
-    printNameAndDogName: function(){
-        const myName = human.name
-        const dogName = human.pet.name
-        debugger;
-        console.log(`my name is ${myName} & my dog is ${dogName}`);
-    },
-    // printJob: () => {function/object/array} - cara baru
-    printJob : (urutan) => {
-        const job = human.jobs[urutan]
-        console.log(`my job is ${job}`);
-    }
-}
-let building = {
-    name: 'batam pos',
-    floor: 21,
-    printNameAndFloor : function(){
-        const name = this.name;
-        const floor = this.floor;
-        console.log(`${name} building have ${floor} floors.`);  
-    },
-    printOther: (othernote) => {
-        console.log(othernote);
-        
-    }
-}
+        name: 'bean',
+        height: 200,
+        weight: 50,
+        live: ['batam', 'singapore', {
+            position: 'director',
+            location: 'batam center',
+        }],
+        pet: {
+            name: 'beanyjr',
+            height: 20,
+            weight: 20,
+            live: ['batam', 'singapore'],
+        },
+        printNameAndDogName: function () {
+            const myName = human.name
+            const dogName = human.pet.name
+            const living = human.live[1]
+            console.log(`my name is ${myName} & my dog is ${dogName} & LIVE IN ${living}`);
 
-// PEMANGGILAN ARRAY
+        },
+        // printJob: () => {function/object/array} - cara baru
+        printJob: (urutan) => {
+            const job = human.jobs[urutan]
+            console.log(`my job is ${job}`);
+        }
+        }
+        // let building = {
+        //     name: 'batam pos',
+        //     floor: 21,
+        //     printNameAndFloor: function () {
+        //         const name =this.name;
+        //         const floor = this.floor;
+        //         console.log(`${name} building have ${floor} floors.`);
+        //     },
+        let building = {
+            name: 'batam pos',
+            floor: 21,
+            printNameAndFloor: () => {
+                const name = building.name;
+                const floor = building.floor;
+                console.log(`${name} building have ${floor} floors.`);
+            },
+            printOther: (othernote) => {
+                console.log(othernote);
+
+            }
+        }
+
+
+        // PEMANGGILAN ARRAY
